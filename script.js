460ee27ac7a422590e41be64c3b3db69cb20bd74
Leandro Costa <148167174+LeandroCosta9513@users.noreply.github.com>
2024-01-20 20:27:22 -0300

    site completo new

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(() => {
+  // MENU ------------------------------
+  $(".nav__menu-btn").on("click", function () {
+    $("#menu").toggleClass("mobile");
+    $("#menu").toggleClass("desktop");
+    $(this).find("i").toggleClass("fa-bars");
+    $(this).find("i").toggleClass("fa-times");
+  });
+  // -----------------------------------
+
+  // EMAIL -----------------------------
+  $("#newslatter-form").submit(function (e) {
+    e.preventDefault();
+    const email = $("#email").val();
+    const emailOptions = {
+      Host: "smtp.elasticemail.com",
+      Username: "jackson.f205@gmail.com",
+      Password: "9DDE1D718AD391B6DB61CE7AA75CB9BF8E9A",
+      To: "contato@studion.com",
+      From: email,
+      Subject: "Por favor me adicione na newsletter",
+      Body: `
+        Olá,
+        
+        Eu gostaria de ser adicionado na newsletter do site.
+        Meu email é ${email}.
+        
+        Obrigado!
+      `,
+    };
+    try {
+      Email.send(emailOptions)
+        .then(() => {
+          alert("Email enviado com sucesso!");
+          location.replace(location.pathname);
+        });
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+  // -----------------------------------
+});
